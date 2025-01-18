6676b8b5656d94d9e0c447cd558ff72b832a68c5
lubomirkamensky <lubomir.kamensky@gmail.com>
2025-01-18 17:25:01 +0100

    bugfixing

--- models/fact/fact.sql
+++ models/fact/fact.sql
@@ -1,21 +1,21 @@
 with source as (
-    select 'sm2_01' as location, sm2_01.* from {{ ref('sm2_01') }} as sm2_01
+    select 'sm2_01' as location, sm2_01.* from {{ source('csv_google','sm2_01') }} as sm2_01
     union all
-    select 'sm2_02' as location, sm2_02.* from {{ ref('sm2_02') }} as sm2_02
+    select 'sm2_02' as location, sm2_02.* from {{ source('csv_google','sm2_02') }} as sm2_02
     union all
-    select 'sm2_03' as location, sm2_03.* from {{ ref('sm2_03') }} as sm2_03
+    select 'sm2_03' as location, sm2_03.* from {{ source('csv_google','sm2_03') }} as sm2_03
     union all
-    select 'sm2_04' as location, sm2_04.* from {{ ref('sm2_04') }} as sm2_04
+    select 'sm2_04' as location, sm2_04.* from {{ source('csv_google','sm2_04') }} as sm2_04
     union all
-    select 'sm2_05' as location, sm2_05.* from {{ ref('sm2_05') }}as  sm2_05
+    select 'sm2_05' as location, sm2_05.* from {{ source('csv_google','sm2_05') }}as  sm2_05
     union all
-    select 'sm2_06' as location, sm2_06.* from {{ ref('sm2_06') }} as sm2_06
+    select 'sm2_06' as location, sm2_06.* from {{ source('csv_google','sm2_06') }} as sm2_06
     union all
-    select 'sm2_07' as location, sm2_07.* from {{ ref('sm2_07') }} as sm2_07
+    select 'sm2_07' as location, sm2_07.* from {{ source('csv_google','sm2_07') }} as sm2_07
     union all
-    select 'sm2_08' as location, sm2_08.* from {{ ref('sm2_08') }} as sm2_08
+    select 'sm2_08' as location, sm2_08.* from {{ source('csv_google','sm2_08') }} as sm2_08
     union all
-    select 'sm2_09' as location, sm2_09.* from {{ ref('sm2_09') }} as sm2_09
+    select 'sm2_09' as location, sm2_09.* from {{ source('csv_google','sm2_09') }} as sm2_09
 ),
 unpivoted as (
     select Date as time, location, 'temp_ambient' as data_key, venkov as data_value from source
