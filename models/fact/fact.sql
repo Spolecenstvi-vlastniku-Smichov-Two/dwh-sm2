a4519e0c5402ba1e24fe9884f10d152b575e9898
lubomirkamensky <lubomir.kamensky@gmail.com>
2025-02-02 10:57:42 +0100

    style

--- models/fact/fact.sql
+++ models/fact/fact.sql
@@ -1,32 +1,35 @@
 with source as (
-    select cast(columns(*) as varchar) from {{ source('csv_google','merged') }} 
+    select cast(columns(*) as varchar) from {{ source('csv_google','merged') }}
 ),
+
 unpivoted as (
     unpivot source
-    on columns(* exclude (Date))
+    on columns(* exclude (date))
     into
-        name data_key_original
-        value data_value
+    name data_key_original
+    value data_value
 ),
+
 mapped as (
     select
-        unpivoted.Date as time,
+        unpivoted.date as "time", --noqa
         mapping.location,
         mapping.data_key,
         unpivoted.data_value
     from unpivoted
-    inner join {{ ref('mapping') }} as mapping
-    on unpivoted.data_key_original = mapping.data_key_original
-    where unpivoted.Date is not null
+    inner join {{ ref('mapping') }} as mapping --noqa
+        on unpivoted.data_key_original = mapping.data_key_original
+    where unpivoted.date is not null
 ),
+
 final as (
-    select 
+    select
         time, --strptime(time, '%d.%m.%Y %H:%M:%S') as time, 
-        location, 
-        data_key, 
-        data_value 
+        location,
+        data_key,
+        data_value
     from mapped
-    union
+    union distinct
     select * from {{ source('csv_google','fact_original') }}
 )
 
