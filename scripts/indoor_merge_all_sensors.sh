76c978d83c14c570a35f9221055d1d6da254b9a0
lubomirkamensky <lubomir.kamensky@gmail.com>
2025-07-06 09:41:26 +0200

    develop: updated gitignore to propagate sh files from scripts folder

---
+++ scripts/indoor_merge_all_sensors.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+output="./gdrive/all_sensors_merged.csv"
+first=1
+total_lines=0
+
+rm -f "$output"
+
+for file in ./latest/ThermoProSensor_export_*.csv; do
+  echo "📄 Zpracovávám: $file"
+
+  location=$(basename "$file" | awk -F'_' '{print $3}')
+  echo "   - location: $location"
+
+  # Smazat popisnou hlavičku
+  awk 'NR==1 && /^Timestamp/ {next} {print}' "$file" > tmp && mv tmp "$file"
+
+  # Smazat BOM
+  sed -i 's/\xEF\xBB\xBF//g' "$file"
+
+  # Opravit koncovou čárku v hlavičce
+  sed -i '1s/,[[:space:]]*$//' "$file"
+
+  if [ $first -eq 1 ]; then
+    # První soubor: přidej hlavičku Datetime
+    awk -F',' -v loc="$location" 'BEGIN{OFS=","}
+    NR==1 {
+      print "Datetime", $3, $4, "location"
+      next
+    }
+    NF {
+      split($1, d, "/")
+      datetime = d[3] "-" sprintf("%02d", d[1]) "-" sprintf("%02d", d[2])
+
+      split($2, t, ":")
+      hour = t[1]; minute = t[2]
+      if (length(hour)==1) hour = "0" hour
+      datetime = datetime " " hour ":" minute ":00"
+
+      print datetime, $3, $4, loc
+    }' "$file" > "$output"
+
+    lines=$(awk 'NR>1 && NF' "$file" | wc -l)
+    first=0
+  else
+    # Další soubory: bez hlavičky
+    awk 'NR==1 && /^Timestamp/ {next} NR==1 {next} {print}' "$file" > tmp && mv tmp "$file"
+
+    awk -F',' -v loc="$location" 'BEGIN{OFS=","}
+    NF {
+      split($1, d, "/")
+      datetime = d[3] "-" sprintf("%02d", d[1]) "-" sprintf("%02d", d[2])
+
+      split($2, t, ":")
+      hour = t[1]; minute = t[2]
+      if (length(hour)==1) hour = "0" hour
+      datetime = datetime " " hour ":" minute ":00"
+
+      print datetime, $3, $4, loc
+    }' "$file" >> "$output"
+
+    lines=$(awk 'NR>1 && NF' "$file" | wc -l)
+  fi
+
+  echo "   - Přidáno řádků: $lines"
+  total_lines=$((total_lines + lines))
+done
+
+echo "✅ Hotovo!"
+echo "Celkem sloučeno řádků: $total_lines"
+echo "Výstupní soubor: $output"
