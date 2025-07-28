324bba5c37f9c1a688534080513bcd72ee106ee0
lubomirkamensky <lubomir.kamensky@gmail.com>
2025-07-28 09:04:31 +0200

    Update indoor_merge_all_sensors.sh
    added detection of Date format MM/DD/2025 or DD/MM/2025

--- scripts/indoor_merge_all_sensors.sh
+++ scripts/indoor_merge_all_sensors.sh
@@ -14,6 +14,43 @@ if ! ls ./latest/ThermoProSensor_export_*.csv 1> /dev/null 2>&1; then
   exit 0
 fi
 
+# Funkce pro určení formátu data
+guess_date_format() {
+  local file="$1"
+  local d m
+  local dmy=0
+  local mdy=0
+  local unknown=0
+
+  local current_month=$(date +%m | sed 's/^0*//')  # např. 07 → 7
+
+  awk -F',' 'NR % 1000 == 1 && NR > 1 && $1 ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/ {print $1}' "$file" | while read -r line; do
+    d=$(echo "$line" | cut -d'/' -f1)
+    m=$(echo "$line" | cut -d'/' -f2)
+
+    if ((10#$d > 12)); then
+      dmy=$((dmy + 1))
+    elif ((10#$m > 12)); then
+      mdy=$((mdy + 1))
+    else
+      # nejednoznačný případ → porovnání s aktuálním měsícem
+      if ((10#$d == current_month)); then
+        mdy=$((mdy + 1))
+      elif ((10#$m == current_month)); then
+        dmy=$((dmy + 1))
+      else
+        unknown=$((unknown + 1))
+      fi
+    fi
+  done
+
+  if (( dmy > mdy )); then
+    echo "DMY"
+  else
+    echo "MDY"
+  fi
+}
+
 # Pro každý soubor
 for file in ./latest/ThermoProSensor_export_*.csv; do
   echo "📄 Zpracovávám: $file"
@@ -30,21 +67,27 @@ for file in ./latest/ThermoProSensor_export_*.csv; do
   # Odstranit koncovou čárku v hlavičce
   sed -i '1s/,[[:space:]]*$//' "$file"
 
+  # Detekce formátu data
+  date_format=$(guess_date_format "$file")
+  echo "   - Formát datumu: $date_format"
+
   if [ $first -eq 1 ]; then
-    # První soubor: přidej natvrdo hlavičku
-    awk -F',' -v loc="$location" 'BEGIN{OFS=","}
+    # První soubor: přidej hlavičku
+    awk -F',' -v loc="$location" -v fmt="$date_format" 'BEGIN{OFS=","}
     NR==1 {
       print "Datetime,Temperature_Celsius,Relative_Humidity(%),Location"
       next
     }
     NF && $1 ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/ && $2 ~ /^[0-9]{1,2}:[0-9]{2}$/ {
       split($1, d, "/")
-      datetime = d[3] "-" sprintf("%02d", d[1]) "-" sprintf("%02d", d[2])
+      day = (fmt == "DMY" ? d[1] : d[2])
+      month = (fmt == "DMY" ? d[2] : d[1])
+      year = d[3]
 
       split($2, t, ":")
       hour = t[1]; minute = t[2]
       if (length(hour)==1) hour = "0" hour
-      datetime = datetime " " hour ":" minute ":00"
+      datetime = year "-" sprintf("%02d", month) "-" sprintf("%02d", day) " " hour ":" minute ":00"
 
       print datetime, $3, $4, loc
     }' "$file" > "$output"
@@ -55,15 +98,17 @@ for file in ./latest/ThermoProSensor_export_*.csv; do
     # Další soubory: bez hlavičky
     awk 'NR==1 && /^Timestamp/ {next} NR==1 {next} {print}' "$file" > tmp && mv tmp "$file"
 
-    awk -F',' -v loc="$location" 'BEGIN{OFS=","}
+    awk -F',' -v loc="$location" -v fmt="$date_format" 'BEGIN{OFS=","}
     NF && $1 ~ /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/ && $2 ~ /^[0-9]{1,2}:[0-9]{2}$/ {
       split($1, d, "/")
-      datetime = d[3] "-" sprintf("%02d", d[1]) "-" sprintf("%02d", d[2])
+      day = (fmt == "DMY" ? d[1] : d[2])
+      month = (fmt == "DMY" ? d[2] : d[1])
+      year = d[3]
 
       split($2, t, ":")
       hour = t[1]; minute = t[2]
       if (length(hour)==1) hour = "0" hour
-      datetime = datetime " " hour ":" minute ":00"
+      datetime = year "-" sprintf("%02d", month) "-" sprintf("%02d", day) " " hour ":" minute ":00"
 
       print datetime, $3, $4, loc
     }' "$file" >> "$output"
